52fd786ce1f59514ec837ac6d5a2b9bc005b9564
KSH3535 <157556983+KSH3535@users.noreply.github.com>
2024-06-24 21:32:22 +0900

    Front / import store 경로 변경

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Root from './router/root';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './store/store';
 import ExamPage from './pages/user/ExamPage';
 
 function App() {

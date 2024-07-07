68388d7b195a26afe341d6802477bd7168bb1f95
KSH3535 <157556983+KSH3535@users.noreply.github.com>
2024-07-08 05:54:46 +0900

    Front / 럭키드로우 페이지 생성

---
+++ front/src/hooks/useLuckyDraw.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useLuckyDraw = () => {
+    const [drawItems, setDrawItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchDrawItems = async () => {
+            try {
+                const response = await axios.get('/luckydraw');
+                const data = response.data;
+
+                if (Array.isArray(data)) {
+                    setDrawItems(data);
+                } else {
+                    throw new Error("Unexpected response format");
+                }
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDrawItems();
+    }, []);
+
+    return { drawItems, loading, error };
+};
+
+export default useLuckyDraw;

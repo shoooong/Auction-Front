4b2aacf66ad02b3d1321a9f4def22ebfba716a5b
KSH3535 <157556983+KSH3535@users.noreply.github.com>
2024-07-11 17:41:03 +0900

    Front / feat : 마이페이지 관심 탭 변경

--- front/src/components/mypage/MypageBookmark.jsx
+++ front/src/components/mypage/MypageBookmark.jsx
@@ -6,12 +6,16 @@ import "styles/bookmark.css";
 
 const MypageBookmark = () => {
     return (
-        <div className="container">
-            <div className="product-tab">
+        <div className="container"> 
+            <div className="mypage-tab buying">
                 <Tabs defaultValue={1}>
                     <TabsList className="bookmark-tab">
-                        <Tab value={1}>관심 상품</Tab>
-                        <Tab value={2}>관심 스타일</Tab>
+                        <Tab value={1}>
+                            <span className="bookmark-tab-span">관심 상품</span>
+                        </Tab>
+                        <Tab value={2}>
+                            <span>관심 스타일</span>
+                        </Tab>
                     </TabsList>
                     <TabPanel value={1}><BookmarkProduct /></TabPanel>
                     <TabPanel value={2}>Second page</TabPanel>

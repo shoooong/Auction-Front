258f94b59233a08db40e18c5c19837023749bb45
GollllDong <wjdqjatnwkd@naver.com>
2024-07-11 14:30:19 +0900

    front / 대분류 페이지 구현중

---
+++ front/src/pages/product/TopProduct.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+    IconButton,
+    Button,
+    Box,
+} from "@mui/material";
+import BookmarkOff from "../../assets/images/bookmark-off.svg";
+import BookmarkOn from "../../assets/images/bookmark-on.svg";
+import Product from "./Product";
+import "../../styles/product.css";
+import tempImg from "../../assets/images/feed6.png"
+
+const TopProduct = () => {
+    const [like, setLike] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [visibleProducts, setVisibleProducts] = useState(5);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/products/상의');
+                const data = response.data.map(product => ({
+                    productId: product.productId,
+                    productImg: tempImg,
+                    productBrand: product.productBrand,
+                    productName: product.productName,
+                    productLike: product.productLike,
+                    modelNum: product.modelNum,
+                    productMinPrice: product.productMinPrice,
+                }));
+                setProducts(data);
+            } catch (error) {
+                console.error('Error fetching products: ', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    const loadMoreProducts = () => {
+        setVisibleProducts(prevVisibleProducts => prevVisibleProducts + 5);
+    };
+
+    return (
+        <div className="container">
+            <Box className="box">
+                <Box className="product-title-box">
+                    <h2 className="product-title">구매입찰이 많은 상품</h2>
+                </Box>
+                <Box className="product-wrap no-wrap">
+                    {products.slice(0, visibleProducts).map((product, index) => (
+                        <Product
+                            key={index}
+                            rank={index + 1} // 순위 전달
+                            productImg={product.productImg}
+                            productBrand={product.productBrand}
+                            productName={product.productName}
+                            productLike={product.productLike}
+                            modelNum={product.modelNum}
+                            productMinPrice={product.productMinPrice}
+                            setLike={setLike}
+                            like={like}
+                        />
+                    ))}
+                </Box>
+
+                {visibleProducts < products.length && (
+                    <div className="text-center">
+                        <Button className="add-btn" onClick={loadMoreProducts}>더보기</Button>
+                    </div>
+                )}
+            </Box>
+        </div>
+    );
+};
+
+export default TopProduct;
--- front/src/router/mainRouter.jsx
+++ front/src/router/mainRouter.jsx
@@ -4,111 +4,105 @@ import ClothesCategory from "layout/ClothesCategory";
 import LifeCategory from "layout/LifeCategory";
 import MypageCategory from "layout/MypageCategory";
 import ServiceCategory from "layout/ServiceCategory";
-import clothesRouter from "./clothesRouter";
+
 import lifeRouter from "./lifeRouter";
 import userRouter from "./userRouter";
 import mypageRouter from "./mypageRouter";
 import serviceRouter from "./serviceRouter";
 
-import Style from "pages/style/Style";
 import Sample from "pages/Sample";
 import AdminSample from "pages/admin/AdminExample";
 import Shop from "pages/shop/Shop";
 import LuckyDraw from "pages/draw/LuckyDraw";
 import luckyDrawRouter from "./luckyDrawRouter";
-import Event from "pages/event/Event";
-import StyleDetail from "pages/style/StyleDetail";
+import clothes from "pages/clothes/ClothesMain";
 
 const mainRouter = () => {
-    return [
+  return [
+    {
+      path: "sample",
+      element: <Sample />,
+    },
+    // {
+    //     path: "admin",
+    //     element: <AdminSample />,
+    // },
+    {
+      path: "/",
+      element: <MainCategory />,
+      children: [
         {
-            path: "sample",
-            element: <Sample />,
+          path: "/",
+          element: <ClothesCategory />,
         },
-        // {
-        //     path: "admin",
-        //     element: <AdminSample />,
-        // },
         {
-            path: "/",
-            element: <MainCategory />,
-            children: [
-                {
-                    path: "/",
-                    element: <ClothesCategory />,
-                },
-                {
-                    path: "life",
-                    element: <LifeCategory />,
-                },
-                {
-                    path: "/tech",
-                    element: <div>테크</div>,
-                },
-                {
-                    path: "/rank",
-                    element: <div>랭킹</div>,
-                },
-                {
-                    path: "/luckydraw",
-                    element: <LuckyDraw />,
-                },
-                {
-                    path: ":luckyId",
-                    children: luckyDrawRouter(),
-                },
-                {
-                    path: "/event",
-                    element: <Event />,
-                },
-            ],
+          path: "life",
+          element: <LifeCategory />,
         },
+        {
+          path: "/tech",
+          element: <div>테크</div>,
+        },
+        {
+          path: "/rank",
+          element: <div>랭킹</div>,
+        },
+        {
+          path: "/luckydraw",
+          element: <LuckyDraw />,
+        },
+        {
+          path: ":luckyId",
+          children: luckyDrawRouter(),
+        },
+        {
+          path: "/event",
+          element: <div>쿠폰 이벤트~!</div>,
+        },
+      ],
+    },
+    {
+      path: "life",
+      children: lifeRouter(),
+    },
+    {
+      path: "shop",
+      element: <Shop />,
+    },
+    {
+      path: "style",
+      element: <StyleCategory />,
+      children: [
+        {
+          path: "",
+          element: <div>피드</div>,
+        },
+        {
+          path: "rank",
+          element: <div>랭킹</div>,
+        },
+      ],
+    },
+    {
+      path: "service",
+      element: <ServiceCategory />,
+      children: serviceRouter(),
+    },
+    {
+      path: "mypage",
+      element: <MypageCategory />,
+      children: mypageRouter(),
+    },
+    {
+      path: "user",
+      children: userRouter(),
+    },
+    {
+        path : "clothes",
+        element : <clothesMain />,
+    }
 
-        {
-            path: "clothes",
-            children: clothesRouter(),
-        },
-        {
-            path: "life",
-            children: lifeRouter(),
-        },
-        {
-            path: "shop",
-            element: <Shop />,
-        },
-        {
-            path: "style",
-            element: <Style />,
-            children: [
-                {
-                    path: "",
-                    element: <div>피드</div>,
-                },
-                {
-                    path: "rank",
-                    element: <div>랭킹</div>,
-                },
-                {
-                    path: "styledetail",
-                    element: <StyleDetail />,
-                },
-            ],
-        },
-        {
-            path: "service",
-            element: <ServiceCategory />,
-            children: serviceRouter(),
-        },
-        {
-            path: "mypage",
-            element: <MypageCategory />,
-            children: mypageRouter(),
-        },
-        {
-            path: "user",
-            children: userRouter(),
-        },
-    ];
+  ];
 };
 
 export default mainRouter;

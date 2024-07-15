195b712720e3cc738b028767d865608c5fa0cc8a
Sophie-Ahn <sgdream95@gmail.com>
2024-07-15 11:42:51 +0900

    Front / styleFeed.css 삭제

--- front/src/pages/Rank/StyleRanking.jsx
+++ front/src/pages/Rank/StyleRanking.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import StyleRankingList from './StyleRankingList';
-import '../../styles/stylefeed.css';
+import React from "react";
+import StyleRankingList from "./StyleRankingList";
 
 const StyleRanking = () => {
-  return (
-    <div className="style">
-      <header>
-        <h1>스타일 페이지</h1>
-      </header>
-      <StyleRankingList />
-    </div>
-  );
+    return (
+        <div className="style">
+            <header>
+                <h1>스타일 페이지</h1>
+            </header>
+            <StyleRankingList />
+        </div>
+    );
 };
 
 export default StyleRanking;

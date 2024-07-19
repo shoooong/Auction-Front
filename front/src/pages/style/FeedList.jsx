cfdc3d787cb788c53a9e3d588aedb164e881cad2
jeongsik <xwjdtlr@naver.com>
2024-07-19 21:45:17 +0900

    front / Object Storage 이미지 조회 완료

--- front/src/pages/style/FeedList.jsx
+++ front/src/pages/style/FeedList.jsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Feed from './Feed';
 
-import {SERVER_URL} from "api/serverApi";
+import { SERVER_URL } from "api/serverApi";
 
 const FeedList = () => {
   const [feeds, setFeeds] = useState([]);
+
+  const CLOUD_STORAGE_BASE_URL = 'https://kr.object.ncloudstorage.com/push/shooong/';
 
   useEffect(() => {
     const fetchFeeds = async () => {
@@ -15,13 +17,13 @@ const FeedList = () => {
         const data = response.data.map(feed => ({
           id: feed.feedId,
           username: feed.userId ? `User ${feed.userId}` : 'Unknown',
-          image: feed.feedImage,
+          image: `${CLOUD_STORAGE_BASE_URL}${feed.feedImage}`,  // 이미지 URL 조합
           description: feed.feedTitle,
           likes: feed.likeCount
         }));
         setFeeds(data);
       } catch (error) {
-       console.error('Error fetching feeds:', error);
+        console.error('Error fetching feeds:', error);
       }
     };
 

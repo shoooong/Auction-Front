23e1578c138d0b4a1b0515828f7993bcc55149e9
KSH3535 <157556983+KSH3535@users.noreply.github.com>
2024-07-22 05:45:27 +0900

    Front / feat : 마이페이지 계좌관리 CSS 완료

--- front/src/pages/user/mypage/Account.jsx
+++ front/src/pages/user/mypage/Account.jsx
@@ -74,7 +74,7 @@ const Account = () => {
                 <h2 className='title'>계좌 관리</h2>
             </div>
             {account.depositor && !isAdding && !isEditing && (
-                <div>
+                <div className="account-details">
                     <p><strong>예금주</strong> {account.depositor}</p>
                     <p><strong>은행명</strong> {account.bankName}</p>
                     <p><strong>계좌번호</strong> {account.accountNum}</p>
@@ -82,28 +82,32 @@ const Account = () => {
                 </div>
             )}
             {!account.depositor && !isAdding && !isEditing && (
-                <div>
+                <div className="no-account">
                     <p>등록된 계좌가 없습니다.</p>
                     <button type="button" onClick={() => setIsAdding(true)}>추가</button>
                 </div>
             )}
             {(isEditing || isAdding) && (
-                <div className="account-form">
-                    <div>
-                        <label>예금주</label>
-                        <input name="depositor" type="text" value={account.depositor} onChange={handleChange} />
+                <>
+                    <div className="account-form">
+                        <div>
+                            <label>예금주</label>
+                            <input name="depositor" type="text" value={account.depositor} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label>은행명</label>
+                            <input name="bankName" type="text" value={account.bankName} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label>계좌번호</label>
+                            <input name="accountNum" type="text" value={account.accountNum} onChange={handleChange} />
+                        </div>
                     </div>
-                    <div>
-                        <label>은행명</label>
-                        <input name="bankName" type="text" value={account.bankName} onChange={handleChange} />
+                    <div className="account-btn">
+                        <button type="button" onClick={handleSave}>저장</button>
+                        <button type="button" onClick={handleCancel}>취소</button>
                     </div>
-                    <div>
-                        <label>계좌번호</label>
-                        <input name="accountNum" type="text" value={account.accountNum} onChange={handleChange} />
-                    </div>
-                    <button type="button" onClick={handleSave}>저장</button>
-                    <button type="button" onClick={handleCancel}>취소</button>
-                </div>
+                </>
             )}
         </div>
     );

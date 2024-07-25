7a487f357122917bb17490fecf978c3805047edd
jong1uk <106105315+jong1uk@users.noreply.github.com>
2024-07-25 10:10:33 +0900

    Front / order 수정

--- front/src/pages/ordres/Sell.jsx
+++ front/src/pages/ordres/Sell.jsx
@@ -1,11 +1,15 @@
 import prImg from "assets/images/sample.png";
 import postImg from "assets/images/icon-post.png";
-import tossImg from "assets/images/icon-toss.png";
+
 import arrowImg from "assets/images/arrow2.svg";
 import "styles/order.css";
 import useSell from "hooks/useSell";
 import { useEffect, useState } from "react";
 import { Button, Box, Dialog, DialogTitle } from "@mui/material";
+import OrderAddressComponent from "components/OrderAddressComponent";
+import jwtAxios from "pages/user/jwtUtil";
+import { SERVER_URL } from "../../api/serverApi";
+import { useNavigate } from "react-router-dom";
 // import Event from "pages/user/mypage/CouponMain";
 // import Postcode from "components/mypage/Postcode";
 // ------  SDK 초기화 ------
@@ -13,11 +17,56 @@ import { Button, Box, Dialog, DialogTitle } from "@mui/material";
 // import useAddress from "hooks/useAddress";
 
 export default function Sell() {
-    const [open, setOpen] = useState(false);
-    // const [open2, setOpen2] = useState(false);
+    const navigate = useNavigate();
+    const [open2, setOpen2] = useState(false);
+    const [fee, setFee] = useState(0);
+    // const [totalPrice, setTotalPrice] = useState(0);
 
+    // const [open2, setOpen2] = useState(false);
     const { salesBidding, addressInfo } = useSell();
-    console.log(salesBidding);
+
+    const [orderData, setOrderData] = useState({
+        productId: null,
+        price: null,
+        exp: 90,
+        addressId: null,
+    });
+
+    useEffect(() => {
+        if (addressInfo && salesBidding) {
+            const calculatedFee = salesBidding.salesBiddingPrice * 0.04;
+            setFee(calculatedFee);
+            setOrderData((prevData) => ({
+                ...prevData,
+                addressId: addressInfo.addressId,
+                productId: salesBidding.product.productId,
+                price: salesBidding.salesBiddingPrice - calculatedFee,
+                exp: 90,
+            }));
+        }
+    }, [addressInfo, salesBidding]);
+    // console.log(salesBidding);
+    // console.log(addressInfo);
+    // console.log(salesBidding?.product.productId);
+
+    console.log(orderData);
+
+    const handleSell = async () => {
+        try {
+            // 1단계: 입찰 정보 생성
+            const orderResponse = await jwtAxios.post(
+                `${SERVER_URL}/api/bid/salesBidding/register`,
+                orderData
+            );
+
+            // 성공 메시지
+            alert("주문이 성공적으로 생성되었습니다.");
+            navigate("/event");
+        } catch (error) {
+            console.error("주문 생성 중 오류가 발생했습니다.", error);
+            alert("주문 생성 중 오류가 발생했습니다.");
+        }
+    };
 
     return (
         <div className="buy_bg">
@@ -41,6 +90,22 @@ export default function Sell() {
                                 {salesBidding?.product.productSize}
                             </p>
                         </div>
+                    </div>
+                </div>
+                <div className="account_info_area info_area">
+                    <div className="account_info">
+                        <h3 className="title_txt">판매 정산 계좌</h3>
+                        <div className="info_box">
+                            <dt className="buy_title">계좌</dt>
+                            <dd className="desc">우리은행 1002960016078</dd>
+                        </div>
+                        <div className="info_box">
+                            <dt className="buy_title">예금주</dt>
+                            <dd className="desc">정종욱</dd>
+                        </div>
+                    </div>
+                    <div className="account_btn btn_box">
+                        <button className="btn_edit border_box">변경</button>
                     </div>
                 </div>
                 <div className="delivery_info_area info_area">
@@ -86,7 +151,10 @@ export default function Sell() {
                                     </dl>
                                 </div>
                                 <div className="btn_box">
-                                    <button className="btn_edit border_box">
+                                    <button
+                                        className="btn_edit border_box"
+                                        onClick={() => setOpen2(true)}
+                                    >
                                         변경
                                     </button>
                                 </div>
@@ -131,11 +199,11 @@ export default function Sell() {
                         </div>
                     </div>
                 </div>
-                <div className="coupon_info_area info_area">
+                {/* <div className="coupon_info_area info_area">
                     <h3 className="title_txt">할인 혜택</h3>
                     <p className="desc">쿠폰</p>
-                </div>
-                <div className="pay-info-area info_area">
+                </div> */}
+                {/* <div className="pay-info-area info_area">
                     <h3 className="title_txt">결제 방법</h3>
                     <div className="main_title">
                         <p className="desc">일반 결제</p>
@@ -150,9 +218,9 @@ export default function Sell() {
                         카드사 문의 바랍니다. 단, 카드사별 정책에 따라 분할 납부
                         변경 시 수수료가 발생할 수 있습니다.
                     </p>
-                </div>
+                </div> */}
 
-                <div className="final_order_info_area info_area">
+                <div className="final_order_info_area info_area sell_final">
                     <h3 className="title_txt">최종 주문 정보</h3>
                     <div className="order_main_title">
                         <div className="order_item">
@@ -166,9 +234,10 @@ export default function Sell() {
                             <p className="sub_text">검수비</p>
                             <p className="desc">무료</p>
                         </div>
+
                         <div className="order_item">
                             <p className="sub_text">수수료</p>
-                            <p className="desc">8,000원</p>
+                            <p className="desc">{fee?.toLocaleString()}원</p>
                         </div>
                         <div className="order_item">
                             <p className="sub_text">배송비</p>
@@ -178,17 +247,24 @@ export default function Sell() {
                 </div>
                 <div className="order_payment_area info_area">
                     <div className="order_box">
-                        <p className="pay_text16">총 결제금액</p>
+                        <p className="pay_text16">정산금액</p>
                         <p className="pay_text20">
-                            {salesBidding?.salesBiddingPrice.toLocaleString()}원
+                            {orderData?.price?.toLocaleString()}원
                         </p>
                     </div>
                 </div>
                 <div className="final_payment_btn info_area">
                     <div className="pay_btn_box">
-                        <button className="pay_btn">
-                            {salesBidding?.salesBiddingPrice.toLocaleString()}원
-                            • 입찰하기
+                        <button
+                            className="pay_btn"
+                            onClick={handleSell}
+                            disabled={
+                                !orderData.addressId ||
+                                !orderData.productId ||
+                                !orderData.price
+                            }
+                        >
+                            {orderData?.price?.toLocaleString()}원 • 판매하기
                         </button>
                     </div>
                 </div>
@@ -225,6 +301,22 @@ export default function Sell() {
                     </div>
                 </Box>
             </Dialog> */}
+            <Dialog
+                open={open2}
+                onClose={() => setOpen2(false)}
+                PaperProps={{
+                    style: {
+                        textAlign: "center",
+                        width: "520px",
+                        maxWidth: "unset",
+                        height: "600px",
+                        maxHeight: "unset",
+                        // borderRadius: "15px",
+                    },
+                }}
+            >
+                <OrderAddressComponent />
+            </Dialog>
         </div>
     );
 }

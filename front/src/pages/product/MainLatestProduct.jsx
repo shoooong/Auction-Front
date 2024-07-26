554e440a9bd629b5f02e4af34865054990dd63b9
GollllDong <wjdqjatnwkd@naver.com>
2024-07-26 18:23:23 +0900

    fix : 상품 금액 포맷팅

--- front/src/pages/product/MainLatestProduct.jsx
+++ front/src/pages/product/MainLatestProduct.jsx
@@ -27,7 +27,7 @@ const MainLatestProduct = () => {
                     productBrand: product.productBrand,
                     productName: product.productName,
                     modelNum: product.modelNum,
-                    biddingPrice: product.biddingPrice ? product.biddingPrice : product.originalPrice,
+                    biddingPrice: product.biddingPrice ? product.biddingPrice.toLocaleString() : product.originalPrice.toLocaleString(),
                     liked: false, // 초기 좋아요 상태
                     rank: index + 1, // 순위 추가
                 }));
--- front/src/pages/product/MainNewSales.jsx
+++ front/src/pages/product/MainNewSales.jsx
@@ -27,7 +27,7 @@ const MainNewSales = () => {
                     productBrand: product.productBrand,
                     productName: product.productName,
                     modelNum: product.modelNum,
-                    biddingPrice: product.biddingPrice,
+                    biddingPrice: product.biddingPrice ? product.biddingPrice.toLocaleString() : product.originalPrice.toLocaleString(),
                     liked: false, // 초기 좋아요 상태
                     rank: index + 1, // 순위 추가
                 }));

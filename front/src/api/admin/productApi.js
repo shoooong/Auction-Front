3ca4f04c1d3f4c804faf850da37cda95b912b257
sue-23 <85052088+sue-23@users.noreply.github.com>
2024-07-27 19:02:31 +0900

    front/admin 판매상품 페이지네이션 적용

--- front/src/api/admin/productApi.js
+++ front/src/api/admin/productApi.js
@@ -7,22 +7,40 @@ export const CLOUD_STORAGE_BASE_URL =
 // 상품 대분류 및 소분류별 조회
 export const getProductsByDepartment = async (
   mainDepartment,
-  subDepartment = null
+  subDepartment = null,
+  page,
+  pageSize
 ) => {
-  const params = subDepartment ? { subDepartment } : {};
+  // 페이지네이션 및 소분류 파라미터를 설정
+  const params = {
+    page,
+    size: pageSize,
+  };
+
+  // 소분류가 있을 경우 파라미터에 추가
+  if (subDepartment) {
+    params.subDepartment = subDepartment;
+  }
+
+  // 디버깅 로그
   console.log(mainDepartment, subDepartment);
+
+  // 요청 전송
   const res = await jwtAxios.get(`/admin/products/${mainDepartment}`, {
     params,
   });
+
+  // 결과 반환
   return res.data;
 };
-
 // 상품 상세 조회
 export const getProduct = async (modelNum, productSize = null) => {
   const params = productSize ? { productSize } : {};
   const res = await jwtAxios.get(`/admin/products/detailed/${modelNum}`, {
     params,
   });
+  console.log("Product detail API response:", res.data); // 디버깅 로그 추가
+
   console.log(res.data);
 
   return res.data;
